877be5fec03bad9199909aff23e321a3f511fbdd
Amuriana <pollicevanessa@gmail.com>
2020-04-12 12:29:57 -0400

    zendesk not working

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -1,8 +1,8 @@
 class Intervention < ApplicationRecord
-    belongs_to :author, class_name: "Employee"
+    belongs_to :author, class_name: "Employee", optional: true
     belongs_to :customer
     belongs_to :building
-    belongs_to :battery
+    belongs_to :battery, optional: true 
     belongs_to :column, optional: true
     belongs_to :elevator, optional: true
     belongs_to :employee, optional: true
@@ -10,7 +10,7 @@ class Intervention < ApplicationRecord
   
 
 
-  after_save :create_intervention_ticket
+  #after_save :create_intervention_ticket
 
   def create_intervention_ticket
       client = ZendeskAPI::Client.new do |config|
