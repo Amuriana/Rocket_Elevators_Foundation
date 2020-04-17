3501c161789edc65b8d0fe7397752a4f8c9c781b
Amuriana <pollicevanessa@gmail.com>
2020-04-17 18:38:11 -0400

    devise sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true, with: :exception
-    before_action :check_session_validity
-
-  private
-
-  def check_session_validity
-    unless session[:expires_after] && session[:expires_after] > Time.now.to_i
-        new_page_url new_lead_url
-    end
-  end
+    
     
 end
