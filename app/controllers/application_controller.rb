3708f35cc9d8b601c8f15b602a9e604586dad41a
Amuriana <pollicevanessa@gmail.com>
2020-04-17 12:13:39 -0400

    sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true, with: :exception
+    before_action :check_session_validity
+
+  private
+
+  def check_session_validity
+    unless session[:expires_after] && session[:expires_after] > Time.now.to_i
+      redirect_to new_login_url
+    end
+  end
     
 end
