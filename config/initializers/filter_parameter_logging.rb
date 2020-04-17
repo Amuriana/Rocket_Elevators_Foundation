169b63b00c00b753685ad737a5e03f486c121870
Amuriana <pollicevanessa@gmail.com>
2020-04-17 18:33:14 -0400

    Update filter_parameter_logging.rb

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :password_confirmation, :address, :email, :phone, :current_sign_in_ip,
+    :last_sign_in_ip,
+    :ip_address, ]
